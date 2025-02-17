62831b9ad64290a472a10f87c6f4fefe8d32f110
mizuki <0zf12342787019w@au.com>
2025-02-17 22:00:10 +0900

    feat: add fetchArticleById function for fetching article details

--- src/app/lib/microcms.ts
+++ src/app/lib/microcms.ts
@@ -1,15 +1,33 @@
-const API_URL = 'https://gift-tech.microcms.io/api/v1/articles';
+const API_URL = "https://gift-tech.microcms.io/api/v1/articles";
 
 export async function fetchArticles() {
-  const res = await fetch(`${API_URL}`, {
+  const res = await fetch(API_URL, {
     headers: {
-      'X-MICROCMS-API-KEY': process.env.NEXT_PUBLIC_MICROCMS_API_KEY || '',
+      "X-MICROCMS-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY || "",
     },
   });
 
   if (!res.ok) {
-    throw new Error('Failed to fetch articles');
+    throw new Error("Failed to fetch articles");
   }
 
-  return res.json();
+  const data = await res.json(); 
+  console.log("取得した記事データ:", data); 
+
+  return data; 
 }
+
+
+export async function fetchArticleById(id: string) {
+    const res = await fetch(`${API_URL}/${id}`, {
+      headers: {
+        "X-MICROCMS-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY || "",
+      },
+    });
+  
+    if (!res.ok) {
+      return null;
+    }
+  
+    return await res.json();
+  }
